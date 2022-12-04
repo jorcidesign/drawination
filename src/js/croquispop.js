acf55469225e77443ec8d2de9d0c6719160c48a9
jorcidesign <jorci.design@gmail.com>
2022-12-04 12:06:38 -0500

    pan with shift

--- src/js/croquispop.js
+++ src/js/croquispop.js
@@ -1,21 +1,21 @@
 // Initialize croquis
+var myDivContainer = document.querySelector('.ar-1-1')
+var clientHeight = myDivContainer.offsetHeight;
+var clientWidth = myDivContainer.offsetWidth;
+
 var croquis = new Croquis();
 croquis.lockHistory();
 croquis.setCanvasSize(720, 720);
 croquis.addLayer();
-croquis.fillLayer('#fff');
-croquis.addLayer();
-croquis.selectLayer(1);
 croquis.unlockHistory();
 croquis.setToolStabilizeLevel(10);
 croquis.setToolStabilizeWeight(0.35);
 // croquis.setPaintingOpacity(1);
 
-
-let currentToolBrush  = {
-    name: 'brush', 
-    opacity: 1, 
-  }
+let currentToolBrush = {
+    name: 'brush',
+    opacity: 1,
+}
 // imagenes de los pinceles 
 let lineArtBrush = new Image();
 lineArtBrush.src = './assets/brushHard.png'
@@ -28,7 +28,7 @@ sketchBrush.src = './assets/pencil.png'
 
 //*--------------------- Initizalize brushes----------------*//
 //shadow brush 
-var brush= new Croquis.Brush();
+var brush = new Croquis.Brush();
 brush.setSize(24);
 brush.setColor('#000');
 brush.setSpacing(0.1);
@@ -66,14 +66,14 @@ selectSketchBrush.onclick = function () {
     brushSizeSlider.value = brush2.getSize();
     croquis.setPaintingOpacity(window[currentToolBrush.name].getOpacityBrushTool());
     brushOpacitySlider.value = brush2.getOpacityBrushTool() * 100;
-    
+
     console.log(currentToolBrush.name);
-    
+
 }
 
 
 let selectLineArtBrush = document.getElementById('lineart-brush');
-selectLineArtBrush.onclick = function (){
+selectLineArtBrush.onclick = function () {
     croquis.setTool(brush3);
     currentToolBrush.name = 'brush3';
     brushSizeSlider.value = brush3.getSize();
@@ -83,7 +83,7 @@ selectLineArtBrush.onclick = function (){
 }
 
 let selectShadowBrush = document.getElementById('shadow-brush');
-selectShadowBrush.onclick = function (){
+selectShadowBrush.onclick = function () {
     croquis.setTool(brush);
     currentToolBrush.name = 'brush'
     brushSizeSlider.value = brush.getSize();
@@ -104,6 +104,9 @@ canvasArea.appendChild(croquisDOMElement);
 function canvasPointerDown(e) {
     setPointerEvent(e);
     var pointerPosition = getRelativePosition(e.clientX, e.clientY);
+
+
+    // console.log(pointerPosition);
     if (pointerEventsNone)
         canvasArea.style.setProperty('cursor', 'none');
     if (/* e.pointerType === "pen" && */ e.button == 5)
@@ -416,7 +419,7 @@ var brushOpacitySlider = document.getElementById('brush-opacity-slider');
 // var brushAngleSlider = document.getElementById('brush-angle-slider');
 // var brushRotateToDirectionCheckbox = document.getElementById('brush-rotate-to-direction-checkbox');
 brushSizeSlider.value = window[currentToolBrush.name].getSize();
-brushOpacitySlider.value = window[currentToolBrush.name].getOpacityBrushTool()*100;
+brushOpacitySlider.value = window[currentToolBrush.name].getOpacityBrushTool() * 100;
 // brushFlowSlider.value = brush.getFlow() * 100;
 // brushSpacingSlider.value = brush.getSpacing() * 100;
 // brushAngleSlider.value = brush.getAngle();
@@ -446,7 +449,7 @@ brushSizeSlider.onchange = function () {
     // updatePointer();
 }
 brushOpacitySlider.onchange = function () {
-    
+
     window[currentToolBrush.name].setOpacityBrushTool(brushOpacitySlider.value * 0.01);
     console.log(window[currentToolBrush.name].getOpacityBrushTool())
     croquis.setPaintingOpacity(window[currentToolBrush.name].getOpacityBrushTool());
@@ -514,8 +517,8 @@ let downloadButton = document.getElementById('download');
 downloadButton.onclick = function () {
 
     const canvasDownload = document.querySelectorAll('.croquis-layer-canvas');//obtener las canvas layers del croquis
-    let can = canvasDownload.item(0);//layer 1 (whiteBackground)
-    let can2 = canvasDownload.item(1);//layer 2 (painting)
+    // let can = canvasDownload.item(0);//layer 1 (whiteBackground)
+    let can2 = canvasDownload.item(0);//layer 2 (painting)
 
     //merge layers in other hidden canvas for export 
     let can3 = document.getElementById('canvasExport');
@@ -537,6 +540,93 @@ downloadButton.onclick = function () {
 
 }
 
+
+let zoomIn = document.getElementById('zoom-in');
+zoomIn.onclick = function () {
+    // croquis.setCanvasSize(1080, 1080);
+    const canvasDownload = document.querySelectorAll('.croquis-layer-canvas');//obtener las canvas layers del croquis
+    // let can = canvasDownload.item(0);//layer 1 (whiteBackground)
+    let canvas2 = canvasDownload.item(0);//layer 2 (painting)
+    let ctx2 = canvas2.getContext('2d');
+    var target = new Image();
+    
+    target.src = canvas2.toDataURL();
+   
+/*     const resizedDataUri = resizeImage(target, 300);
+    // targetResize.src = resizeImage(target,1080);
+    target.src = resizedDataUri;
+ */  const canvas1 = document.createElement('canvas');
+    const ctx = canvas1.getContext('2d');
+    canvas1.width = 720;
+    canvas1.height = 720;
+
+    
+    ctx.drawImage(canvas2, 0, 0, 1080, 1080);
+    
+    croquis.clearLayer()
+    document.getElementById('brush-shelf').appendChild(canvas1);
+
+    ctx2.drawImage(canvas1,0,0)
+    // ctx2.drawImage(canvas1,0,0,1080,1080);
+    // console.log(targetResize);
+    // croquis.clearLayer();
+    // ctx2.drawImage(resizeImage(target,1080), 0, 0)
+
+}
+
+
+function resizeImage (imgEl, wantedWidth) {
+    const canvas1 = document.createElement('canvas');
+    const ctx = canvas1.getContext('2d');
+  
+    const aspect = imgEl.width / imgEl.height;
+  
+    canvas1.width = wantedWidth;
+    canvas1.height = wantedWidth / aspect;
+  
+    ctx.drawImage(imgEl, 0, 0, canvas1.width, canvas1.height);
+    return canvas1.toDataURL();
+  }
+
+
+  let div = document.querySelectorAll('.croquis-layer-canvas').item(0);
+  let div2 = document.querySelectorAll('.croquis-painting-canvas').item(0);
+  
+  document.addEventListener('keyup', event => {
+    if (event.code === 'KeyW') {
+        
+      let pointerPosition = getRelativePosition(event.clientX, event.clientY);
+      let w = div.clientWidth;
+      let w2 = w * 1.25;//scale factor 
+      console.log(w2);
+      div.style.setProperty('width', w2 + 'px');
+      div.style.setProperty('height', w2 + 'px');
+      div2.style.setProperty('width', w2 + 'px');
+      div2.style.setProperty('height', w2 + 'px');
+   
+
+      
+    }
+  })
+
+
+  document.addEventListener('keyup', event => {
+    if (event.code === 'KeyQ') {
+
+      let w = div.clientWidth;
+      let w2 = w * 0.8;
+      console.log(w2);
+      div.style.setProperty('width', w2 + 'px');
+      div.style.setProperty('height', w2 + 'px');
+      div2.style.setProperty('width', w2 + 'px');
+      div2.style.setProperty('height', w2 + 'px');
+
+    //   brush.move(0,0,w2);
+    }
+  })
+
+
+// 
 /* 
 var movie = "test";
 var movieId = 1;
